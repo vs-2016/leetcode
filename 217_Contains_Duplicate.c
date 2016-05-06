70a87539f6d20ba92a9e8fc65361ca178d7a32ab
vs-2016 <2899291996@qq.com>
2016-05-06 16:33:07 +0800

    217 C solution,Easy

---
+++ 217_Contains_Duplicate.c
@@ -0,0 +1,8 @@
+int cmp(const void *a, const void *b){
+    return *(int *)a-*(int *)b;
+}
+bool containsDuplicate(int* nums, int numsSize) {
+    qsort(nums,numsSize,sizeof(nums[0]),cmp);
+    for(int i=0;i<numsSize-1;i++)if(nums[i]==nums[i+1])return true;
+    return false;
+}
